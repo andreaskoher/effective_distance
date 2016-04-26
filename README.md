f1fb64fba4b0d3c822c5e86211f42a62bedb2361
Andreas Koher <andreas.koher@campus.tu-berlin.de>
2016-04-26 23:05:36 +0200

    add files

---
+++ README.md
@@ -0,0 +1,106 @@
+
+# Effective Distances on Complex Networks
+
+The effective distance is an alternative distance metric, which takes into account the local topology of the network and the fluxes along the edges. It has been introduced to predict the time that an infection needs in order to spread from a given source node to a specific target.
+The different approaches are based on the intuition, that large fluxes and few neighbors decrease the transmission time to an adjacent node.
+
+We provide a set of methods, which allow to compare different approaches:
+
+#### 1. Pathogens can spread along multiple paths
+Gautreau, A., Barrat, A. and Barthelemy, M., _Global disease spread: Statistics and estimation of arrival times_, J. Theor. Biol. __251__, 3, 509 (2008)
+
+#### 2. Only the most dominant path is considered
+Gautreau, A., Barrat, A. and Barthelemy, M., _Global disease spread: Statistics and estimation of arrival times_, J. Theor. Biol. __251__, 3, 509 (2008)  
+Brockmann, D. and Helbing, D., _The hidden geometry of complex, network-driven contagion phenomena_, Science __342__, 6164, 1337 (2013)
+
+#### 3. Random Walk based approach
+Iannelli, F., Koher, A., Hoevel, P. and Sokolov, I.M. _Effective Distances in Complex Networks_ (in preparation)
+
+---
+# Required Software
+
+Please download the files either using the link or, open a terminal and type
+
+```
+git clone ...git
+```
+The package uses functionalities from numpy, scipy and networkx. You can check your versions against those, which I used by executing the script in your terminal:
+
+```
+$: python effective_distance
+
+
+Effective Distances
+---------
+Required modules:
+Python:   tested for: 2.7.11.  Yours: 2.7.11
+numpy:    tested for: 1.11.0.  Yours: 1.11.0
+scipy:    tested for: 0.17.0.  Yours: 0.17.0
+networkx: tested for: 1.9.1.   Yours: 1.9.1
+```
+
+If your version differs, it is still likely to work anyway.
+
+---
+# Step 1: Import the package
+
+In order to import the package into your python progrann, you will can put the files into the same directory.
+Alternatively, make them visible to python by typing adding the directory to the `PYTHONPATH` variable or enter the following command to the beginning of your python script: 
+```python
+from sys import path
+path.append('<directory/to/effective_distance.py>')
+```
+We can now import the package into the python programm:
+```python
+import effective_distance as ed
+```
+
+---
+# Step 2: Load the network
+
+
+We assume, that the mobility network is stored in a comma seperated file (.csv) following the convention SOURCE, TARGET, FLUX. The node IDs are interpreted as integers and have to run from 0 to number_of_nodes - 1. The fluxes have to be positive and will be saved as float numbers.
+```python
+myEffectiveDistance = ed.EffectiveDistances("data/US_largest500_airportnetwork.csv")
+```
+The graph is now stored in the attribute myEffectiveDistance.graph as a NetworkX type DiGraph. In order to avoid singularities in later calculations, the giant strongly connected component has been stored only.
+
+# Step 3: Effective Distances
+
+We provide three methods to calculate the effective distance, as introduced in the beginning. If the source and target is specified a number is returned. Otherwise, if one is set to `None` or both an array is returned.
+
+```python
+# Dominant path effective distance
+DPED = myEffectiveDistance.get_dominant_path_distance(source, target)
+
+# Multiple path effective distance with different cut-off lengths
+cutoff = range(5)
+MPED = []
+for c in cutoff:
+    MPED.append(
+        myEffectiveDistance.get_multiple_path_distance(source, target, cutoff=c)
+        )
+
+#Random walk effective distance
+RWED = myEffectiveDistance.get_random_walk_distance(source, target)
+```
+
+# Step 4: Plot the results
+
+We can use the commonly used matplotlib package in order to display the results:
+
+```python
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+plt.plot(0,DPED, 'r*')
+plt.plot(cutoff,MPED, 'ko')
+plt.plot([0,cutoff[-1]], [RWED,RWED], 'k-')
+plt.xlim(left=cutoff[0]-.5, right=cutoff[-1]+.5)
+plt.xlabel("topological path length")
+plt.ylabel("effective distance")
+plt.title("Effective Distance from node "+str(source)+" to "+str(target))
+plt.show()
+```
+
+<img src="effective_distances.png" alt="some_text"> 
---
+++ effective_distance.py
@@ -0,0 +1,405 @@
+import numpy as np
+from numpy import genfromtxt, log, array, exp, save
+from networkx import DiGraph, shortest_path_length, adjacency_matrix, shortest_path, all_simple_paths, strongly_connected_component_subgraphs
+from scipy.linalg import inv
+
+
+###############################################################################
+
+class EffectiveDistances:
+    def __init__(self, fname="", verbose=True, **kwargs):
+        assert isinstance(fname,str)
+        self.graph = None
+        self.nodes = 0
+        self.dominant_path_distance = None
+        self.multiple_path_distance = None
+        self.random_walk_distance = None
+        
+        if len(fname) > 0:
+            self.graph = self.load(fname, verbose=verbose, **kwargs)
+    
+    def __str__():
+        pass
+    
+    def load(self,fname, verbose=True, **kwargs):
+        """
+        Load a data file. The expected data format is three columns 
+        (comma seperated by default) with source, target, flux.
+        No header should be included and the node IDs have to run contuously 
+        from 0 to Number_of_nodes-1.
+
+        Parameters
+        ----------
+            fname : str
+                Path to the file
+            
+            verbose : bool
+                Print information about the data. True by Default
+                
+            kwargs : dict
+                Default parameters can be changed here. Supported key words are
+                    dtype     : float (default)
+                    delimiter : ","   (default)
+        
+            return_graph : bool
+                If True, the graph is returned (False by default).
+                
+        Returns:
+        --------
+            The graph is saved internally in self.graph.
+                
+        """    
+        dtype     = kwargs["dtype"] if "dtype" in kwargs else float
+        delimiter = kwargs["delimiter"] if "delimiter" in kwargs else ","
+        source, target, flux = genfromtxt(fname, delimiter=delimiter, dtype=dtype, unpack=True)
+        nodes  = set(source) | set(target)
+        self.nodes = len(nodes)
+        lines  = len(flux)
+        if set(range(self.nodes)) != nodes:
+            new_node_ID = {old:new for new,old in enumerate(nodes)}
+            map_new_node_ID = np.vectorize(new_node_ID.__getitem__)
+            source = map_new_node_ID(source)
+            target = map_new_node_ID(target)
+            if verbose:
+                print "\nThe node IDs have to run continuously from 0 to Number_of_nodes-1."
+                print "Node IDs have been changed according to the requirement.\n-----------------------------------\n"
+                
+        
+        if verbose:
+            print 'Lines: ',lines , ', Nodes: ', self.nodes
+            print '-----------------------------------\nData Structure:\n\nsource,    target,    weight \n'
+            for ii in range(7):            
+                print "%i,       %i,       %1.2e" %(source[ii], target[ii], flux[ii])
+            print '-----------------------------------\n'
+        
+        
+        G = DiGraph()         # Empty, directed Graph
+        G.add_nodes_from(range(self.nodes))
+        for ii in range(lines):
+            u, v, w = int(source[ii]), int(target[ii]), flux[ii]
+            if u != v: # ignore self loops
+                assert not G.has_edge(u,v), "Edge appeared twice - not supported"                    
+                G.add_edge(u,v,weight=w)
+            else:
+                if verbose:
+                    print "ignore self loop at node", u
+
+        symmetric = True
+        for s,t,w in G.edges(data=True):
+            w1 = G[s][t]["weight"]
+            try:
+                w2 = G[t][s]["weight"]
+            except KeyError:
+                symmetric = False
+                G.add_edge(t,s,{"weight":w1})
+                w2 = w1
+            if w1 != w2:
+                symmetric = False
+                G[s][t]["weight"] += G[t][s]["weight"]
+                G[t][s]["weight"]  = G[s][t]["weight"]
+        if verbose:
+            if not symmetric:
+                print "The network has been symmetrisized."
+        
+        
+        ccs = strongly_connected_component_subgraphs(G)
+        ccs = sorted(ccs, key=len, reverse=True)
+        
+        G_GSCC = ccs[0]
+        if G_GSCC.number_of_nodes() != G.number_of_nodes():
+            G = G_GSCC
+            if verbose:
+                print "\n--------------------------------------------------------------------------"
+                print "The network has been restricted to the giant strongly connected component."
+        self.nodes = G.number_of_nodes()
+        
+        
+        
+        
+        for u, v, data in G.edges_iter(data=True):
+            weight = G.out_degree(u,weight='weight')
+            data['transition_rate'] = 1.*data['weight']/weight
+        
+        
+        for u, v, data in G.edges_iter(data=True):
+            data['effective_distance'] = 1. - log(data['transition_rate'])
+        
+        if verbose:
+            print "\n--------------------------------------------------------------------------"
+            print "\nnode ID, out-weight,   normalized out-weight,  sum of effective distances \n "
+            for ii in range(7):
+                out_edges = G.out_edges(ii, data=True)
+                out_weight, effective_distance, transition_rate = 0, 0, 0
+                for u, v, data in out_edges:
+                    out_weight          += data["weight"]
+                    effective_distance  += data["effective_distance"]
+                    transition_rate     += data["transition_rate"]
+                print "  %i       %1.2e           %2.3f                 %1.2e " %(ii,out_weight, transition_rate, effective_distance)
+            print "\n ... graph is saved in self.graph"
+        return G
+        
+    ###############################################################################
+    
+    def get_dominant_path_distance(self, source=None, target=None, parameter=1, saveto=""):
+        """
+        Compute the (dominant path) effective distance:
+        Gautreau, A, Barrat, A. and Barthelemy, M., Global disease spread: 
+        Statistics and estimation of arrival times, Journal of Theoretical Biology 251, 3, 509 (2008)
+
+        Parameters
+        ----------
+             source : int or None
+                If source is None, the distances from all nodes to the target is calculated
+                Otherwise the integer has to correspond to a node index
+            
+            target : int or None
+                If target is None, the distances from the source to all other nodes is calculated
+                Otherwise the integer has to correspond to a node index
+                
+            parameter : float
+                compound parameter which includes the infection and recovery rate alpha and beta, respectively, 
+                the mobility rate kappa and the Euler-Mascheroni constant lambda:
+                    log[ (alpha-beta)/kappa - lambda ]
+        
+            saveto : string
+                If empty, the result is saved internally in self.dominant_path_distance           
+                
+        Returns:
+        --------
+            dominant_path_distance : ndarray or float
+                If source and target are specified, a float value is returned that specifies the distance.
+                
+                If either source or target is None a numpy array is returned.
+                The position corresponds to the node ID. 
+                shape = (Nnodes,)
+                
+                If both are None a numpy array is returned.
+                Each row corresponds to the node ID. 
+                shape = (Nnodes,Nnodes)
+        """    
+        
+        assert (isinstance(parameter,float) or isinstance(parameter,int)) and parameter > 0
+        assert isinstance(saveto,str)
+        assert self.graph != None, "Load graph first."        
+        
+        if parameter != 1:
+            for n,m,data in G.edges_iter(data=True):
+                data['effective_distance'] = parameter - log(data['transition_rate'])
+        
+        DPED_dic = shortest_path_length(self.graph, source=source, target=target, weight="effective_distance")
+        
+        if source is None and target is None:
+            DPED = np.array([DPED_dic[s].values() for s in xrange(self.nodes)]).transpose()
+        elif (source is None) != (target is None):
+            DPED = array(DPED_dic.values())
+        else:
+            DPED = DPED_dic
+        if saveto is not "":
+            save( saveto, DPED )
+        else:
+            return DPED
+        
+        
+    ###############################################################################
+    
+    def get_random_walk_distance(self, source=None, target=None, parameter=1, saveto=""):
+        """
+        Compute the random walk effective distance:
+        F. Iannelli, A. Koher, P. Hoevel, I.M. Sokolov (in preparation)
+        
+        Parameters
+        ----------
+             source : int or None
+                If source is None, the distances from all nodes to the target is calculated
+                Otherwise the integer has to correspond to a node index
+            
+            target : int or None
+                If target is None, the distances from the source to all other nodes is calculated
+                Otherwise the integer has to correspond to a node index
+                
+            parameter : float
+                compound parameter which includes the infection and recovery rate alpha and beta, respectively, 
+                the mobility rate kappa and the Euler-Mascheroni constant lambda:
+                    log[ (alpha-beta)/kappa - lambda ]
+        
+            saveto : string
+                If empty, the result is saved internally in self.dominant_path_distance           
+                
+        Returns:
+        --------
+            random_walk_distance : ndarray or float
+                If source and target are specified, a float value is returned that specifies the distance.
+                
+                If either source or target is None a numpy array is returned.
+                The position corresponds to the node ID. 
+                shape = (Nnodes,)
+                
+                If both are None a numpy array is returned.
+                Each row corresponds to the node ID. 
+                shape = (Nnodes,Nnodes)
+        """           
+        
+        assert (isinstance(parameter,float) or isinstance(parameter,int)) and parameter > 0
+        assert isinstance(saveto,str)
+        assert self.graph != None, "Load graph first."        
+        
+        P = array(adjacency_matrix(self.graph, weight="transition_rate").todense())
+        assert np.all(np.isclose(P.sum(axis=1), 1, rtol=1e-15)), "The transition matrix has to be row normalized"
+        
+        if target is not None:
+            targets = [target]
+        else:
+            targets = range(self.nodes)
+        
+        eye = np.eye( self.nodes-1,self.nodes-1 )*exp(parameter)
+        RWED = np.zeros( (self.nodes, self.nodes) )
+        for t in targets: #go through all possible target nodes
+            P_min = np.delete(P,t,0) # remove the column, which corresponds to the target node
+            P_min = np.delete(P_min,t,1) # and the row. P_min is the minor Matrix 
+            p_min = P[:,t]  # take the column: jump prob. from any node to the target
+            p_min = np.delete(p_min,t) # and remove one entry
+            RWED[t,:] = -np.log( np.insert( np.dot( inv( eye-P_min),p_min),t,1))
+        
+        if source is not None and target is not None:
+            RWED = RWED[target, source]
+        elif (source is None) != (target is None):
+            if source is None:
+                RWED = RWED[target,:]
+            else:
+                RWED = RWED[:,source]
+        
+        if saveto is not "":
+            save( saveto, RWED )
+        else:
+            return RWED
+        
+    
+    ###############################################################################
+    
+    def get_multiple_path_distance(self, source=None, target=None, parameter=1, cutoff=0, saveto="", verbose=False):
+        """
+        Compute the multiple path effective distance:
+        Gautreau, A, Barrat, A. and Barthelemy, M., Global disease spread:
+        Statistics and estimation of arrival times, Journal of Theoretical Biology 251, 3, 509 (2008)
+        
+        Parameters
+        ----------
+             source : int or None
+                If source is None, the distances from all nodes to the target is calculated
+                Otherwise the integer has to correspond to a node index
+            
+            target : int or None
+                If target is None, the distances from the source to all other nodes is calculated
+                Otherwise the integer has to correspond to a node index
+                
+            parameter : float
+                compound parameter which includes the infection and recovery rate alpha and beta, respectively, 
+                the mobility rate kappa and the Euler-Mascheroni constant lambda:
+                    log[ (alpha-beta)/kappa - lambda ]
+        
+            save : string
+                If empty, the result is saved internally in self.dominant_path_distance      
+            
+            verbose : bool
+                Print progress if set as True (False by default)
+                
+        Returns:
+        --------
+            dominant_path_distance : ndarray or float
+                If source and target are specified, a float value is returned that specifies the distance.
+                
+                If either source or target is None a numpy array is returned.
+                The position corresponds to the node ID. 
+                shape = (Nnodes,)
+                
+                If both are None a numpy array is returned.
+                Each row corresponds to the node ID. 
+                shape = (Nnodes,Nnodes)
+        """
+        
+        assert (isinstance(parameter,float) or isinstance(parameter,int)) and parameter > 0
+        assert isinstance(saveto,str)
+        assert self.graph != None, "Load graph first."  
+        
+        if source is None:
+            sources = range(self.nodes)
+        else:
+            sources = [source]
+        if target is None:
+            targets = range(self.nodes)
+        else:
+            targets = [target]
+        
+        MPED_dic = {}
+        for s in sources:
+            if verbose:
+                print s, "out of", self.nodes
+            MPED_dic[s] = np.zeros((self.nodes,))
+            for t in targets:
+                if s != t:
+                    shortest = len(shortest_path(self.graph, source=s, target=t, weight=None))-1
+                    paths = all_simple_paths(self.graph, source=s, target=t, cutoff=shortest+cutoff)
+                    psum = 0
+                    for path in paths:
+                        n = len(path)-1
+                        prob = np.prod([self.graph[path[ii]][path[ii+1]]["transition_rate"] for ii in range(n)])
+                        psum += prob*exp(-parameter*n)
+                    MPED_dic[s][t] = -np.log(psum)
+        
+        if source is not None and target is not None:
+            MPED = MPED_dic[source][target]
+        elif source is None and target is None:
+            MPED = np.array([MPED_dic[s] for s in xrange(self.nodes)]).transpose()
+        else:
+            if target is None:
+                MPED = MPED_dic[source]
+            else:
+                MPED = np.array([MPED_dic[s][target] for s in xrange(self.nodes)])
+        
+        if saveto is not "":
+            save( saveto, MPED )
+        else:
+            return MPED
+        
+###############################################################################
+    
+if __name__ == "__main__":
+    
+    # CHECK VERSIONS 
+    vers_python0 = '2.7.11'
+    vers_numpy0  = '1.11.0'
+    vers_scipy0  = '0.17.0'
+    vers_netx0   = '1.9.1'
+    
+    from sys import version_info
+    from scipy import __version__ as vers_scipy    
+    from networkx import __version__ as vers_netx
+    vers_python = '%s.%s.%s' % version_info[:3]
+    vers_numpy  = np.__version__
+    
+    
+    print '\n---------'
+    print '---------'
+    print '---------'
+    print '---------'
+    print 'Effective Distances'
+    print '---------'
+    print 'Required modules:'
+    print 'Python:   tested for: %s.  Yours: %s'    % (vers_python0, vers_python)
+    print 'numpy:    tested for: %s.  Yours: %s'    % (vers_numpy0, vers_numpy)
+    print 'scipy:    tested for: %s.  Yours: %s'    % (vers_scipy0, vers_scipy)
+    print 'networkx: tested for: %s.   Yours: %s'    % (vers_netx0, vers_netx)
+    print '--------'
+    print '--------'
+    print '--------'
+    print '--------\n'
+    
+    G = EffectiveDistances("data/US_largest500_airportnetwork.csv", verbose=False, dtype=int)
+    #distances = G.get_dominant_path_distance(source=1, target=100, parameter=1, saveto="")
+    #print "Dominant Path Effective Distance :", distances
+    distances = G.get_multiple_path_distance(source=10, target=100, parameter=1, cutoff=3, saveto="")
+    print "Multiple Path Effective Distance :", distances
+    #distances = G.get_random_walk_distance(source=1, target=100, parameter=1, saveto="")
+    #print "Random Walk Effective Distance   :", distances
+    
+    
